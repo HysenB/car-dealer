6469cc042fc0d60744fdfd3989b5d38571a9898d
HysenB <hysi2000@live.com>
2025-03-23 02:07:00 +0100

    created user, taxonomy, session, iamge, customer and classifed prisma models

---
+++ src/lib/prisma.ts
@@ -0,0 +1,10 @@
+import { PrismaClient } from "@prisma/client/extension";
+
+const globalFormPrisma = global as unknown as { prisma: PrismaClient }
+function makeClient() {
+    return new PrismaClient({ log: ['error', 'info', 'warn'] })
+
+}
+
+export const prisma = globalFormPrisma.prisma || makeClient();
+if (process.env.NODE_ENV !== 'production') globalFormPrisma.prisma = prisma;
